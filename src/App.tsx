0e2fb0e713b67afd03cc4cc7e8d5e59ef6ee151f
Daniel <sjbby@naver.com>
2023-07-18 15:16:27 +0900

    init: vite 세팅

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Layout from "./components/grid-layout";
+
+function App() {
+  return (
+    <>
+      <Layout />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/grid-layout/index.tsx
@@ -0,0 +1,37 @@
+import { Responsive, WidthProvider, Layout, Layouts } from "react-grid-layout";
+
+const layout = [
+  { i: "daily-unique-users", x: 0, y: 0, w: 3, h: 1 },
+  { i: "daily-visits", x: 3, y: 0, w: 3, h: 1 },
+  { i: "daily-active-users", x: 0, y: 0, w: 6, h: 1 },
+  { i: "top-referral", x: 0, y: 0, w: 3, h: 1 },
+  { i: "country", x: 3, y: 0, w: 3, h: 1 },
+];
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+function GridLayout() {
+  const onLayoutChange = (layout: Layout[], layouts: Layouts) => {
+    console.log(layout, layouts);
+  };
+
+  return (
+    <ResponsiveGridLayout
+      layouts={{ lg: layout }}
+      width={1000}
+      breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+      cols={{ lg: 6, md: 3, sm: 3, xs: 2, xxs: 1 }}
+      rowHeight={250}
+      onLayoutChange={onLayoutChange}
+      isResizable
+    >
+      <figure key="daily-unique-users">일별 유저</figure>
+      <figure key="daily-visits">daily-visits</figure>
+      <figure key="daily-active-users">daily-active-users</figure>
+      <figure key="top-referral">top-referral</figure>
+      <figure key="country">country</figure>
+    </ResponsiveGridLayout>
+  );
+}
+
+export default GridLayout;
